aefc834a04383b304d219bfa044166f6ef07a97b
Akash <akash.sharma@cibc.com>
2022-07-19 12:20:59 -0400

    New demo file created

---
+++ src/main/java/com/cibc/api/training/accounts/handler/AccountsGetHandler.java
@@ -0,0 +1,15 @@
+package com.cibc.api.training.accounts.handler;
+
+import com.networknt.handler.LightHttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountsGetHandler implements LightHttpHandler {
+    
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        exchange.endExchange();
+    }
+}
